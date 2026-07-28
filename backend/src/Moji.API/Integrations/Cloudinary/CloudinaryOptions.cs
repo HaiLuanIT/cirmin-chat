71a9a1c07a378b3d898ca897b43010ad3697be0d
luanlh <hailuanit31032003@gmail.com>
2026-07-28 22:25:38 +0700

    feat(backend): implement user avatar upload with validation and Cloudinary integration
    - Add `UploadUserAvatarResponse` model for upload responses.
    - Create `ImageValidatorHelper` to validate supported image formats (JPEG, PNG, WebP).
    - Integrate `CloudinaryImageStorageService` for avatar storage using Cloudinary.
    - Add avatar-related request and validation models in the API layer.
    - Update `User` entity to include optimistic concurrency control (`RowVersion`).
    - Add migration for `RowVersion` field in the `Users` table.

---
+++ backend/src/Moji.API/Integrations/Cloudinary/CloudinaryOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace Moji.API.Integrations.Cloudinary;
+
+public class CloudinaryOptions
+{
+    public const string SectionName = "Cloudinary";
+
+    public required string CloudName { get; init; }
+
+    public required string ApiKey { get; init; }
+
+    public required string ApiSecret { get; init; }
+
+    public void Validate()
+    {
+        if (string.IsNullOrEmpty(CloudName)) throw new ArgumentException("CloudName is required");
+        if (string.IsNullOrEmpty(ApiKey)) throw new ArgumentException("ApiKey is required");
+        if (string.IsNullOrEmpty(ApiSecret)) throw new ArgumentException("ApiSecret is required");
+    }
+}
--- backend/src/Moji.DataAccess/Entities/User.cs
+++ backend/src/Moji.DataAccess/Entities/User.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Moji.DataAccess.Commons;
 
 namespace Moji.DataAccess.Entities;
@@ -5,21 +6,23 @@ namespace Moji.DataAccess.Entities;
 public class User : BaseEntity
 {
     public Guid Id { get; set; }
-    
+
     public string Username { get; set; }
-    
+
     public string HashedPassword { get; set; }
-    
+
     public string? Email { get; set; }
-    
+
     public string FullName { get; set; }
-    
+
     public string? AvatarUrl { get; set; }
-    
+
     public string? AvatarId { get; set; }
-    
+
     public string? Bio { get; set; }
-    
+
+    [Timestamp] public uint RowVersion { get; set; }
+
     //navigation
     public virtual ICollection<ConversationMember> Conversations { get; set; } = new List<ConversationMember>();
 }
